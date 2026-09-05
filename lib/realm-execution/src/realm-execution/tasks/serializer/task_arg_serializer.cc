0751e03024d56976b88e9608dddf5ce197aff815
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-09-04 20:57:23 -0700

    Realm experiment 1: split out the invariant portion of task args and intern on a per-process basis to avoid serializing.

--- lib/realm-execution/src/realm-execution/tasks/serializer/task_arg_serializer.cc
+++ lib/realm-execution/src/realm-execution/tasks/serializer/task_arg_serializer.cc
@@ -5,7 +5,7 @@ namespace FlexFlow {
 
 using T = jsonable_value_type<0>;
 
-template std::string serialize_task_args(T const &);
+template std::vector<std::uint8_t> serialize_task_args(T const &);
 
 template T deserialize_task_args(void const *, size_t);
 
